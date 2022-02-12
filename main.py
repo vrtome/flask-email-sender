0d4ee2ee6e137219594f2ab2d8f551918234cc11
Vitor Rodrigues <vitorr661@gmail.com>
2022-02-12 18:04:32 -0300

    seventh post fix

--- main.py
+++ main.py
@@ -20,22 +20,23 @@ def email():
         addr = request.remote_addr
         print("rawrr")
         print(addr)
-        emailHead = request.form['subject']
+        emailHead = request.form.get['subject']
         print(emailHead)
-        emailBody = request.form['text']
-        emailSender = request.form['sender']
-        emailRecipient = request.form['recipient']
-        emailPassword = request.form['password']
-        emailUpload = request.form['upload']
+        emailBody = request.form.get['text']
+        emailSender = request.form.get['sender']
+        emailRecipient = request.form.get['recipient']
+        emailPassword = request.form.get['password']
+        emailUpload = request.form.get['upload']
         print(type(emailUpload))
         se.send_email(emailHead, emailBody, emailSender, emailRecipient,
                       emailPassword, emailUpload)
         return redirect(url_for('response'))
     else:
+        print("chwddd")
         emailHead = request.args.get('subject')
         emailBody = request.args.get('text')
-        render_template('email.html')
-        return redirect(url_for('response'))
+
+        return render_template('email.html')
 
 
 if __name__ == '__main__':
