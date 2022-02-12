dd89c8a4ead4efd0709ae1ceb4bb426402140d27
Vitor Rodrigues <vitorr661@gmail.com>
2022-02-12 17:33:22 -0300

    Post fix 3

--- main.py
+++ main.py
@@ -18,23 +18,25 @@ def response():
 def email():
     if request.method == 'POST':
         addr = request.remote_addr
+        print("rawrr")
         print(addr)
-        emailHead = request.form.get['subject']
+        emailHead = request.form['subject']
+        print(emailHead)
         emailBody = request.form['text']
-        emailSender = request.form.get['sender']
-        emailRecipient = request.form.get['recipient']
-        emailPassword = request.form.get['password']
-        emailUpload = request.form.get['upload']
+        emailSender = request.form['sender']
+        emailRecipient = request.form['recipient']
+        emailPassword = request.form['password']
+        emailUpload = request.form['upload']
         print(type(emailUpload))
         se.send_email(emailHead, emailBody, emailSender, emailRecipient,
                       emailPassword, emailUpload)
         redirect(url_for('response'))
-        return jsonify(result={"status": 200})
+        return redirect(url_for('response'))
     else:
         emailHead = request.args.get('subject')
         emailBody = request.args.get('text')
         render_template('email.html')
-        return jsonify(result={"status": 200})
+        return redirect(url_for('response'))
 
 
 if __name__ == '__main__':
