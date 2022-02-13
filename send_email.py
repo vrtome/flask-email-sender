e39cfdae0b3f6e4d1033a37e718caf867305d8a4
Vitor Rodrigues <vitorr661@gmail.com>
2022-02-13 20:26:03 -0300

    filepath fix 1

--- send_email.py
+++ send_email.py
@@ -28,7 +28,7 @@ def send_email(subject, body, sender, recipient, senderPassword, file):
 
     message.attach(part1)
     message.attach(part2)
-
+    print(f'File path for file: {os.path.realpath(filename)}')
     filename = file
     if filename != "":
         with open(os.path.realpath(filename), "rb") as attachment:
