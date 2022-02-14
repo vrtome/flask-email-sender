ee75a32c90541fa6b34970382a97e3ec942fc0c1
Vitor Rodrigues <vitorr661@gmail.com>
2022-02-13 22:29:27 -0300

    filepath fix 4

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ from distutils.log import debug
 from flask import Flask
 from flask import render_template
 from flask import request
-from flask import redirect, url_for, jsonify
+from flask import redirect, url_for, jsonify, flash
 import send_email as se
 
+UPLOAD_FOLDER = '/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route('/response')
@@ -41,7 +45,21 @@ def email():
         hmmpassword = "password: " + emailPassword
         print(hmmpassword)
 
-        emailUpload = request.form['upload']
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            #return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            #return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            #return redirect(url_for('download_file', name=filename))
+        emailUpload = filename  #request.form['upload']
         hmmupload = "upload: " + emailUpload
         print(hmmupload)
 
